58ef73b9f04833152471455c86d4c61ce680f28f
mhnn99 <mihneabiceps3@gmail.com>
2024-04-06 18:50:03 +0300

    test

---
+++ backend/proiectTPI/src/main/java/com/example/proiecttpi/ProiectTpiApplication.java
@@ -0,0 +1,13 @@
+package com.example.proiecttpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProiectTpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProiectTpiApplication.class, args);
+	}
+
+}
---
+++ backend/proiectTPI/src/test/java/com/example/proiecttpi/ProiectTpiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proiecttpi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProiectTpiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
